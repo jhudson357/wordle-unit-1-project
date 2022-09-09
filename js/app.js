29c4447c1b8a102f417c95026291df574a1a0b53
James Hudson <jmh357@comcast.net>
2022-09-09 12:34:49 -0500

    add keyboard key divs to HTML and create cached elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('sanity check')
+/*-------------------------------- Constants --------------------------------*/
+const keys = [
+  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+  'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK'
+]
+
+
+/*-------------------------------- Variables --------------------------------*/
+let randomWord, row, guesses, winner
+
+
+/*------------------------ Cached Element References ------------------------*/
+const boardEl = document.querySelector('#board-container')
+const keyboardEl = document.querySelector('#keyboard-container')
+console.log(keyboardEl)
+console.log(boardEl)
+
+/*----------------------------- Event Listeners -----------------------------*/
+//keyboardEl.addEventListener('')
+
+
+/*-------------------------------- Functions --------------------------------*/
