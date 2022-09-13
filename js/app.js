eb4121870b6b292078c903ee2f9a729e7a786923
James Hudson <jmh357@comcast.net>
2022-09-12 22:26:43 -0500

    change square elements ids' to be classes in HTML and update className to classList.add in JS

--- js/app.js
+++ js/app.js
@@ -3,25 +3,27 @@ import { getSecretWord, words } from "../data/words.js";
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let secretWord, row, numGuesses, winner, letter, keyPressed
+let secretWord, row, numGuesses, winner, letter, keyPressed, ms
 let guess = []
 
 
 /*------------------------ Cached Element References ------------------------*/
-const boardEl = document.querySelector('#board-container')
+// const boardEl = document.querySelector('#board-container')
 const keyboardEl = document.querySelector('#keyboard-container')
 const messageEl = document.querySelector('#message')
 const resetBtnEl = document.querySelector('#reset-btn')
-const squareEl = document.querySelectorAll('#board-square')
 const firstRowKeys = document.querySelector('#first-row').children
 const secondRowKeys = document.querySelector('#second-row').children
 const thirdRowKeys = document.querySelector('#third-row').children
+const squareEl = document.querySelectorAll('.board-square')
+console.log(squareEl, 'length')
 
+console.log(firstRowKeys[0].id)
 
 /*----------------------------- Event Listeners -----------------------------*/
 keyboardEl.addEventListener('click', virtualKeyboardGuess)
 resetBtnEl.addEventListener('click', startGame)
-document.addEventListener('keydown', physicalKeyboardGuess)
+document.addEventListener('keyup', physicalKeyboardGuess)
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -30,6 +32,7 @@ startGame()
 
 function startGame() {
   numGuesses = -1
+  ms = 0
   resetBtnEl.style.display = 'none'
   messageEl.style.display = 'none'
   keyboardEl.style.display = ''
@@ -42,7 +45,7 @@ function startGame() {
 function clearBoard() {
   for(let i=0; i<squareEl.length; i++) {
     squareEl[i].textContent = ''
-    squareEl[i].className = ''
+    squareEl[i].className = 'board-square'
   }
 }
 
@@ -137,13 +140,13 @@ function checkWordValidity() {
   let guessedWord = guess.join('').toLowerCase()
   console.log(guessedWord)
   if(words.includes(guessedWord)) {
-    console.log('word is valid')
+    // console.log('word is valid')
     checkGuess()
     guess = []
   } else {
-    console.log('word is NOT valid')
+    // console.log('word is NOT valid')
     messageEl.style.display = ''
-    messageEl.textContent = 'Not a valid word'
+    messageEl.textContent = 'Not in word list'
     setTimeout(function() {
       messageEl.style.display = 'none'
     }, 1500)
@@ -152,77 +155,102 @@ function checkWordValidity() {
 
 function checkGuess() {
   let secretWordArray = secretWord.toUpperCase().split('')
+  
+  flipTiles()
   for(let i=0; i<5; i++) {
+    let squareLetter = squareEl[i].textContent
+    console.log(squareLetter, 'square letter')
+
+
     if(secretWordArray.includes(guess[i])) {
       if(guess[i] === secretWordArray[i]) {
         // GREEN LETTER
+        flipTiles()
         if(numGuesses === 4){
-          squareEl[i].className = 'green'
-          squareEl[i].style.animation = 'flip 1s ease '
-          document.getElementById(`${squareEl[i].textContent}`).className = 'green'
+          squareEl[i].classList.add('green')
+          console.log(squareEl[i].className)
+          document.getElementById(`${squareEl[i].textContent}`).classList.add('green')
         } else if(numGuesses === 9) {
-          squareEl[i+5].className = 'green'
+          squareEl[i+5].classList.add('green')
           document.getElementById(`${squareEl[i+5].textContent}`).className = 'green'
         } else if(numGuesses === 14) {
-          squareEl[i+10].className = 'green'
+          squareEl[i+10].classList.add('green')
           document.getElementById(`${squareEl[i+10].textContent}`).className = 'green'
         } else if(numGuesses === 19) {
-          squareEl[i+15].className = 'green'
+          squareEl[i+15].classList.add('green')
           document.getElementById(`${squareEl[i+15].textContent}`).className = 'green'
         } else if(numGuesses === 24) {
-          squareEl[i+20].className = 'green'
+          squareEl[i+20].classList.add('green')
           document.getElementById(`${squareEl[i+20].textContent}`).className = 'green'
         } else if(numGuesses === 29) {
-          squareEl[i+25].className = 'green'
+          squareEl[i+25].classList.add('green')
           document.getElementById(`${squareEl[i+25].textContent}`).className = 'green'  
         }    
       } else {
         // YELLOW LETTER
+        flipTiles()
         if(numGuesses === 4){
-          squareEl[i].className = 'yellow'
+          squareEl[i].classList.add('yellow')
           if(document.getElementById(`${squareEl[i].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i].textContent}`).className = 'yellow'}
         } else if(numGuesses === 9) {
-          squareEl[i+5].className = 'yellow'
+          squareEl[i+5].classList.add('yellow')
           if(document.getElementById(`${squareEl[i+5].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i+5].textContent}`).className = 'yellow'}
         } else if(numGuesses === 14) {
-          squareEl[i+10].className = 'yellow'
+          squareEl[i+10].classList.add('yellow')
           if(document.getElementById(`${squareEl[i+10].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i+10].textContent}`).className = 'yellow'}
         } else if(numGuesses === 19) {
-          squareEl[i+15].className = 'yellow'
+          squareEl[i+15].classList.add('yellow')
           if(document.getElementById(`${squareEl[i+15].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i+15].textContent}`).className = 'yellow'}
         } else if(numGuesses === 24) {
-          squareEl[i+20].className = 'yellow'
+          squareEl[i+20].classList.add('yellow')
           if(document.getElementById(`${squareEl[i+20].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i+20].textContent}`).className = 'yellow'}
         } else if(numGuesses === 29) {
-          squareEl[i+25].className = 'yellow'
+          squareEl[i+25].classList.add('yellow')
           if(document.getElementById(`${squareEl[i+25].textContent}`).className !== 'green') {document.getElementById(`${squareEl[i+25].textContent}`).className = 'yellow'}  
         }
       }
     } else {
       // GRAY LETTER
-      if(numGuesses === 4){
-        squareEl[i].className = 'gray'
+      flipTiles()
+      if(numGuesses === 4) {
+        squareEl[i].classList.add('gray')
         document.getElementById(`${squareEl[i].textContent}`).className = 'gray'
       } else if(numGuesses === 9) {
-        squareEl[i+5].className = 'gray'
+        squareEl[i+5].classList.add('gray')
         document.getElementById(`${squareEl[i+5].textContent}`).className = 'gray'
       } else if(numGuesses === 14) {
-        squareEl[i+10].className = 'gray'
+        squareEl[i+10].classList.add('gray')
         document.getElementById(`${squareEl[i+10].textContent}`).className = 'gray'
       } else if(numGuesses === 19) {
-        squareEl[i+15].className = 'gray'
+        squareEl[i+15].classList.add('gray')
         document.getElementById(`${squareEl[i+15].textContent}`).className = 'gray'
       } else if(numGuesses === 24) {
-        squareEl[i+20].className = 'gray'
+        squareEl[i+20].classList.add('gray')
         document.getElementById(`${squareEl[i+20].textContent}`).className = 'gray'
       } else if(numGuesses === 29) {
-        squareEl[i+25].className = 'gray'
+        squareEl[i+25].classList.add('gray')
         document.getElementById(`${squareEl[i+25].textContent}`).className = 'gray'  
       }
     }
   }
   isWinner()
 }
+
+function flipTiles() {
+  // squareEl[0].style.animation = 'flip 1s ease 0ms'
+  // squareEl[1].style.animation = 'flip 1s ease 100ms'
+  // squareEl[2].style.animation = 'flip 1s ease 200ms'
+  // squareEl[3].style.animation = 'flip 1s ease 300ms'
+  // squareEl[4].style.animation = 'flip 1s ease 40ms'
+  if(numGuesses === 4) {
+    ms = 0
+    for(let i=0; i<5; i++) {
+      squareEl[i].style.animation = `flip 1s ease ${ms}ms`
+      ms += 300
+    }
+  }
+}
+
 
 function isWinner() {
   if(guess.join('') === secretWord.toUpperCase()) {
@@ -243,3 +271,8 @@ function isWinner() {
 // score count
 // allow keyboard to function 
 // only highlight one color square
+
+
+
+
+// STOP UNDOING HERE
